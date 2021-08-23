38f7f2d27623b6019b83adad325ef6022f4edb47
shin <zerll9100@gmail.com>
2021-08-24 01:09:34 +0900

    Chapter 1 완료

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -1,20 +1,26 @@
 package test;
 
 import main.Account;
+import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.*;
 
 public class AccountTest {
 
+    private Account account;
+
+    @Before
+    public void setup(){
+        account = new Account(10000);
+    }
+
     @Test
     public void testAccount() throws Exception{
-        Account account = new Account(10000);
     }
 
     @Test
     public void testGetBalance() throws Exception{
-        Account account = new Account(10000);
         assertEquals("10000원으로 계좌 생성 후 잔고 조회", 10000, account.getBalance());
 
         account = new Account(1000);
@@ -22,6 +28,18 @@ public class AccountTest {
 
         account = new Account(0);
         assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void testDeposit() throws Exception{
+        account.deposit(1000);
+        assertEquals(11000, account.getBalance());
+    }
+
+    @Test
+    public void testWithdraw() throws Exception{
+        account.withdraw(1000);
+        assertEquals(9000, account.getBalance());
     }
 
 }
